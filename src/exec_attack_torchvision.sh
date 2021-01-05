03d9e3444671c27ce3e52a505121108b57cf2241
ginevracoal <ginevracoal@gmail.com>
2021-01-05 14:41:32 +0100

    new exec out

--- src/exec_attack_torchvision.sh
+++ src/exec_attack_torchvision.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 
-MODEL_NAME="resnet" # resnet, alexnet, vgg
+MODEL_NAME="vgg" # resnet, alexnet, vgg
 DATASET_NAME="animals10" # imagenette, imagewoof, animals10, hymenoptera
-BAYESIAN="True"
+BAYESIAN="False"
 INFERENCE="svi" # laplace, svi
 TRAIN="True"
-ATTACK="True"
-N_SAMPLES=1 #00
-ITERS=1
+ATTACK="False"
+N_SAMPLES=100
+ITERS=2
 ATTACK_METHOD="fgsm"
 DEVICE="cpu"
 
 source ../venv/bin/activate
 
+DATE=$(date '+%Y-%m-%d')
 TIME=$(date +%H:%M:%S)
 TESTS="../experiments/"
 
 if [ "${BAYESIAN}" = "True" ]; then
-    SAVEDIR="${MODEL_NAME}_${DATASET_NAME}_${INFERENCE}_iters=${ITERS}"
-	OUT="${TESTS}${SAVEDIR}/${TIME}_${MODEL_NAME}_${DATASET_NAME}_${INFERENCE}_iters=${ITERS}_${ATTACK_METHOD}.txt"
+    SAVEDIR="${MODEL_NAME}_redBNN_${DATASET_NAME}_${INFERENCE}_iters=${ITERS}"
+	OUT="${TESTS}${SAVEDIR}/${MODEL_NAME}_${DATASET_NAME}_${INFERENCE}_iters=${ITERS}_${ATTACK_METHOD}.txt"
 else
-    SAVEDIR="${MODEL_NAME}_${DATASET_NAME}_iters=${ITERS}"
-	OUT="${TESTS}${SAVEDIR}/${TIME}_${MODEL_NAME}_${DATASET_NAME}_iters=${ITERS}_${ATTACK_METHOD}.txt"
+    SAVEDIR="${MODEL_NAME}_baseNN_${DATASET_NAME}_iters=${ITERS}"
+	OUT="${TESTS}${SAVEDIR}/${MODEL_NAME}_${DATASET_NAME}_iters=${ITERS}_${ATTACK_METHOD}.txt"
 fi
 
 mkdir -p "${TESTS}${SAVEDIR}"
 
+echo "=== exec date time ${DATE} ${TIME} ===" >> $OUT
+
 python3 attack_torchvision_networks.py --savedir=$SAVEDIR --model=$MODEL_NAME  --dataset=$DATASET_NAME \
 		--bayesian=$BAYESIAN --inference=$INFERENCE --train=$TRAIN --attack=$ATTACK --iters=$ITERS \
-		--attack_method=$ATTACK_METHOD --n_samples=$N_SAMPLES --device=$DEVICE &> $OUT
+		--attack_method=$ATTACK_METHOD --n_samples=$N_SAMPLES --device=$DEVICE &>> $OUT
 
 deactivate
