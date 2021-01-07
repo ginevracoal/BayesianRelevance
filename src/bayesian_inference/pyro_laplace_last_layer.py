59d5844f5196ba13dcb0366d674f600724b8e806
ginevracoal <ginevracoal@gmail.com>
2021-01-07 10:33:58 +0100

    torchvision redBNN laplace refactoring

---
+++ src/bayesian_inference/pyro_laplace_last_layer.py
@@ -0,0 +1,158 @@
+import time
+
+import torch
+import torch.nn.functional as nnf
+softplus = torch.nn.Softplus()
+
+import pyro
+from pyro import poutine
+import pyro.optim as pyroopt
+from pyro.infer import SVI, Trace_ELBO, TraceMeanField_ELBO, Predictive
+from pyro.distributions import OneHotCategorical, Normal, Categorical, Uniform, Delta
+from pyro.contrib.autoguide import AutoLaplaceApproximation
+
+DEBUG=False
+
+def model(bayesian_network, x_data, y_data):
+
+    net = bayesian_network.basenet
+    w, b, w_name, b_name = bayesian_network._last_layer(net)
+
+    w.requires_grad=True
+    b.requires_grad=True
+
+    outw_prior = Normal(loc=torch.zeros_like(w), scale=torch.ones_like(w))
+    outb_prior = Normal(loc=torch.zeros_like(b), scale=torch.ones_like(b))
+
+    outw = pyro.sample(w_name, outw_prior)
+    outb = pyro.sample(b_name, outb_prior)
+
+    # print(outw.shape, outb.shape)
+
+    with pyro.plate("data", len(x_data)):
+        output = bayesian_network.rednet(x_data).squeeze()
+        yhat = torch.matmul(output, outw.t()) + outb 
+        lhat = nnf.log_softmax(yhat, dim=-1)
+        cond_model = pyro.sample("obs", Categorical(logits=lhat), obs=y_data)
+    
+    return cond_model
+
+def train(bayesian_network, dataloaders, criterion, optimizer, device, num_iters=10, is_inception=False):
+
+    bayesian_network.to(device)
+
+    network = bayesian_network.basenet
+    bayesian_network.model = model
+    since = time.time()
+
+    guide = AutoLaplaceApproximation(bayesian_network.model)
+    elbo = Trace_ELBO()
+    svi = SVI(bayesian_network.model, guide, optimizer, loss=elbo)
+
+    val_acc_history = []
+
+    for epoch in range(num_iters):
+
+        loss=0.0
+
+        print('Epoch {}/{}'.format(epoch, num_iters - 1))
+        print('-' * 10)
+
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                network.train()  # Set model to training mode
+            else:
+                network.eval()  # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            for inputs, labels in dataloaders[phase]:
+
+                inputs, labels  = inputs.to(device), labels.to(device)
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    # Get model outputs and calculate loss
+                    # Special case for inception because in training it has an auxiliary output. In train
+                    #   mode we calculate the loss by summing the final output and the auxiliary output
+                    #   but in testing we only consider the final output.
+
+                    loss += svi.step(bayesian_network, x_data=inputs, y_data=labels)
+                    bayesian_network.delta_guide = guide.get_posterior()
+
+                    outputs = bayesian_network.forward(inputs)
+                    _, preds = torch.max(outputs, 1)
+
+                    if DEBUG:
+                        print(bayesian_network.basenet.state_dict()['conv1.weight'][0,0,:5])
+                        print(pyro.sample("posterior", bayesian_network.delta_guide)[:5]) # = guide.loc
+
+                running_loss += loss * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            # if DEBUG:
+            #     print(list(poutine.trace(self.guide).get_trace(inputs).nodes.keys()))
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            val_acc_history.append(epoch_acc)
+
+        print()
+
+    bayesian_network.laplace_posterior = guide.laplace_approximation(bayesian_network, inputs, labels)
+
+    print("\nLearned variational params:\n")
+    print(pyro.get_param_store().get_all_param_names())
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+
+    return val_acc_history
+
+def forward(bayesian_network, inputs, n_samples, seeds):
+
+    out_batch = bayesian_network.rednet(inputs).squeeze(3).squeeze(2)
+
+    if hasattr(bayesian_network, 'laplace_posterior'):
+        # after training use Laplace posterior approximation
+
+        preds = []  
+        for seed in seeds:
+            pyro.set_rng_seed(seed)
+            posterior = pyro.sample("posterior", bayesian_network.laplace_posterior)
+            out_w = posterior['fc.weight']
+            out_b = posterior['fc.bias']
+            output_probs = torch.matmul(out_batch, out_w.t()) + out_b
+            preds.append(output_probs)
+
+        output_probs = torch.stack(preds)
+
+    else:
+        # during training use delta function at MAP estimate
+        map_weights = pyro.sample("posterior", bayesian_network.delta_guide)
+
+        layer_size = out_batch.shape[1]
+        out_w = map_weights[:bayesian_network.num_classes*layer_size]
+        out_w = out_w.reshape(bayesian_network.num_classes, layer_size)
+        out_b = map_weights[bayesian_network.num_classes*layer_size:]
+
+        output_probs = torch.matmul(out_batch.squeeze(), out_w.t()) + out_b
+        output_probs = output_probs.unsqueeze(0)
+
+        if DEBUG:
+            print("out_w[:5] =", out_w[:5])
+
+    return output_probs
+
+def save(path, filename):
+    save_to_pickle(self.laplace_posterior, path, filename)
+
+def load(path, filename):
+    self.laplace_posterior = load_from_pickle(path+filename)
+
+def to(bayesian_network, device):
+    if hasattr(bayesian_network, "laplace_posterior"):
+        bayesian_network.laplace_posterior = bayesian_network.laplace_posterior.to(device)
--- src/networks/torchvision/redBNN.py
+++ src/networks/torchvision/redBNN.py
@@ -10,6 +10,7 @@ from utils.savedir import *
 from utils.data import *
 
 import bayesian_inference.pyro_svi_last_layer as pyro_svi
+import bayesian_inference.pyro_laplace_last_layer as pyro_laplace
 
 DEBUG=False
 
@@ -29,11 +30,10 @@ class torchvisionBNN(torchvisionNN):
         self.rednet = self.rednet.to(device)
 
         if self.inference=="svi":
-
             pyro_svi.to(device)
 
-        # if hasattr(self, "laplace_posterior"):
-        #     self.laplace_posterior = self.laplace_posterior.to(device)
+        if self.inference=="laplace":
+            pyro_laplace.to(self, device)
 
     def initialize_model(self, model_name, num_classes, feature_extract, use_pretrained=True):
         """
@@ -83,99 +83,13 @@ class torchvisionBNN(torchvisionNN):
         is_inception: flag for Inception v3 model
         """
         if self.inference == "svi":
-            pyro_svi.train(self, dataloaders, criterion, optimizer, 
-                                                device, num_iters, is_inception)
+            pyro_svi.train(self, dataloaders, criterion, optimizer, device, num_iters, is_inception)
 
         elif self.inference == "laplace":
-            self.train_laplace(dataloaders, criterion, optimizer, device, num_iters, is_inception)
+            pyro_laplace.train(self, dataloaders, criterion, optimizer, device, num_iters, is_inception)
 
         else:
             raise NotImplementedError
-
-    def train_laplace(self, dataloaders, criterion, optimizer, device, num_iters=10, is_inception=False):
-
-        self.to(device)
-
-        network = self.basenet
-        since = time.time()
-
-        guide = AutoLaplaceApproximation(self.model)
-        elbo = Trace_ELBO()
-        svi = SVI(self.model, guide, optimizer, loss=elbo) # todo: check this
-
-        val_acc_history = []
-
-        # best_network_wts = copy.deepcopy(network.state_dict())
-        # best_acc = 0.0
-
-        for epoch in range(num_iters):
-
-            loss=0.0
-
-            print('Epoch {}/{}'.format(epoch, num_iters - 1))
-            print('-' * 10)
-
-            for phase in ['train', 'val']:
-                if phase == 'train':
-                    network.train()  # Set model to training mode
-                else:
-                    network.eval()  # Set model to evaluate mode
-
-                running_loss = 0.0
-                running_corrects = 0
-
-                for inputs, labels in dataloaders[phase]:
-
-                    inputs, labels  = inputs.to(device), labels.to(device)
-
-                    with torch.set_grad_enabled(phase == 'train'):
-                        # Get model outputs and calculate loss
-                        # Special case for inception because in training it has an auxiliary output. In train
-                        #   mode we calculate the loss by summing the final output and the auxiliary output
-                        #   but in testing we only consider the final output.
-
-                        loss += svi.step(x_data=inputs, y_data=labels)
-                        self.delta_guide = guide.get_posterior()
-
-                        outputs = self.forward(inputs)
-                        _, preds = torch.max(outputs, 1)
-
-                        if DEBUG:
-                            print(self.basenet.state_dict()['conv1.weight'][0,0,:5])
-                            print(pyro.sample("posterior", self.delta_guide)[:5]) # = guide.loc
-
-                    running_loss += loss * inputs.size(0)
-                    running_corrects += torch.sum(preds == labels.data)
-
-                # if DEBUG:
-                #     print(list(poutine.trace(self.guide).get_trace(inputs).nodes.keys()))
-
-                epoch_loss = running_loss / len(dataloaders[phase].dataset)
-                epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
-
-                print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
-
-                # # deep copy the model
-                # if phase == 'val' and epoch_acc > best_acc:
-                #     best_acc = epoch_acc
-                #     best_network_wts = copy.deepcopy(network.state_dict())
-                # if phase == 'val':
-                val_acc_history.append(epoch_acc)
-
-            print()
-
-        self.laplace_posterior = guide.laplace_approximation(inputs, labels)
-
-        print("\nLearned variational params:\n")
-        print(pyro.get_param_store().get_all_param_names())
-
-        time_elapsed = time.time() - since
-        print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
-        # print('Best val Acc: {:4f}'.format(best_acc))
-
-        # load best model weights
-        # self.basenet.load_state_dict(best_network_wts)
-        return self.basenet, val_acc_history
 
     def _last_layer(self, net):
 
@@ -198,24 +112,7 @@ class torchvisionBNN(torchvisionNN):
 
         if self.inference=="laplace":
 
-            net = self.basenet
-            w.requires_grad=True
-            b.requires_grad=True
-
-            outw_prior = Normal(loc=torch.zeros_like(w), scale=torch.ones_like(w))
-            outb_prior = Normal(loc=torch.zeros_like(b), scale=torch.ones_like(b))
-
-            outw = pyro.sample(w_name, outw_prior)
-            outb = pyro.sample(b_name, outb_prior)
-
-            # print(outw.shape, outb.shape)
-
-            with pyro.plate("data", len(x_data)):
-                output = self.rednet(x_data).squeeze()
-                yhat = torch.matmul(output, outw.t()) + outb 
-                lhat = nnf.log_softmax(yhat, dim=-1)
-                cond_model = pyro.sample("obs", Categorical(logits=lhat), obs=y_data)
-                return cond_model
+            return pyro_laplace.model(self, x_data, y_data)
 
         elif self.inference=="svi":
 
@@ -233,40 +130,9 @@ class torchvisionBNN(torchvisionNN):
             seeds = list(range(n_samples))
 
         if self.inference=="laplace":
-
-            out_batch = self.rednet(inputs).squeeze(3).squeeze(2)
-
-            if hasattr(self, 'laplace_posterior'):
-                # after training use Laplace posterior approximation
-
-                preds = []  
-                for seed in seeds:
-                    pyro.set_rng_seed(seed)
-                    posterior = pyro.sample("posterior", self.laplace_posterior)
-                    out_w = posterior['fc.weight']
-                    out_b = posterior['fc.bias']
-                    output_probs = torch.matmul(out_batch, out_w.t()) + out_b
-                    preds.append(output_probs)
-
-                output_probs = torch.stack(preds)
-
-            else:
-                # during training use delta function at MAP estimate
-                map_weights = pyro.sample("posterior", self.delta_guide)
-
-                layer_size = out_batch.shape[1]
-                out_w = map_weights[:self.num_classes*layer_size]
-                out_w = out_w.reshape(self.num_classes, layer_size)
-                out_b = map_weights[self.num_classes*layer_size:]
-
-                output_probs = torch.matmul(out_batch.squeeze(), out_w.t()) + out_b
-                output_probs = output_probs.unsqueeze(0)
-
-                if DEBUG:
-                    print("out_w[:5] =", out_w[:5])
+            output_probs = pyro_laplace.forward(self, inputs, n_samples, seeds)
 
         elif self.inference=="svi":
-
             output_probs = pyro_svi.forward(self, inputs, n_samples, seeds)
 
         else:
@@ -282,12 +148,10 @@ class torchvisionBNN(torchvisionNN):
         os.makedirs(os.path.dirname(path), exist_ok=True)
         
         if self.inference=="svi":
-
             pyro_svi.save(path, filename)
 
         elif self.inference=="laplace":
-
-            save_to_pickle(self.laplace_posterior, path, filename)
+            pyro_laplace.save(path, filename)
 
         print("\nSaving: ", path + filename)
 
@@ -296,11 +160,9 @@ class torchvisionBNN(torchvisionNN):
         filename=self.name+"_iters="+str(num_iters)+"_weights.pt"
 
         if self.inference=="svi":
-
             pyro_svi.load(path, filename)
 
         elif self.inference=="laplace":
-
-            self.laplace_posterior = load_from_pickle(path+filename)
+            pyro_laplace.load(path, filename)
 
         self.to(device)
