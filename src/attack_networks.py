softmax lrp bug

src/attack_networks.py
@@ -22,10 +22,13 @@ parser.add_argument("--debug", default=False, type=eval, help="Run script in deb
 parser.add_argument("--device", default='cuda', type=str, help="cpu, cuda")  
 args = parser.parse_args()
 
-n_inputs=200 if args.debug else args.n_inputs
-bayesian_attack_samples=[1,2] if args.debug else [1,10,50]
+n_inputs=100 if args.debug else args.n_inputs
+bayesian_attack_samples=[1,2] if args.debug else [5, 10,50]
 
 print("PyTorch Version: ", torch.__version__)
+
+if args.attack_method=="deepfool":
+  args.device="cpu"
 
 if args.device=="cuda":
     torch.set_default_tensor_type('torch.cuda.FloatTensor')

src/networks/baseNN.py
@@ -51,7 +51,7 @@ class baseNN(nn.Module):
                     "_ep="+str(self.epochs)+"_lr="+str(self.lr)
 
         print("\nbaseNN total number of weights =", sum(p.numel() for p in self.parameters()))
-        self.n_layers = len(list(self.model.children()))+1
+        self.n_layers = len(list(self.model.children()))
         learnable_params = self.model.state_dict()
         self.n_learnable_layers = int(len(learnable_params)/2)
 
@@ -168,7 +168,6 @@ class baseNN(nn.Module):
 
         preds = nn.Sequential(*list(self.model.children())[:layer_idx])(inputs)
         return preds
-        # return nnf.softmax(preds, dim=-1)
 
     def get_logits(self, *args, **kwargs):
         return self.forward(*args, **kwargs)

src/networks/fullBNN.py
@@ -83,7 +83,7 @@ class BNN(PyroModule):
         if self.inference == "svi":
             return name+"_ep="+str(self.epochs)+"_lr="+str(self.lr)
         elif self.inference == "hmc":
-            return name+"_samp="+str(self.n_samples)+"_warm="+str(self.warmup)+\
+            return name+"_samp="+str(self.hmc_samples)+"_warm="+str(self.warmup)+\
                    "_stepsize="+str(self.step_size)+"_numsteps="+str(self.num_steps)
 
     def model(self, x_data, y_data):
@@ -99,8 +99,8 @@ class BNN(PyroModule):
 
         with pyro.plate("data", len(x_data)):
             logits = lifted_module(x_data)
-            lhat = nnf.log_softmax(logits, dim=-1)
-            obs = pyro.sample("obs", Categorical(logits=lhat), obs=y_data)
+            logits = nnf.log_softmax(logits, dim=-1)
+            obs = pyro.sample("obs", Categorical(logits=logits), obs=y_data)
 
     def guide(self, x_data, y_data=None):
 
@@ -161,13 +161,13 @@ class BNN(PyroModule):
             os.makedirs(savedir, exist_ok=True)  
 
             self.posterior_samples=[]
-            for idx in range(self.n_samples):
+            for idx in range(self.hmc_samples):
                 net_copy = copy.deepcopy(self.basenet)
                 fullpath=os.path.join(savedir, filename+"_"+str(idx)+".pt")    
                 net_copy.load_state_dict(torch.load(fullpath))
                 self.posterior_samples.append(net_copy)  
 
-            if len(self.posterior_samples)!=self.n_samples:
+            if len(self.posterior_samples)!=self.hmc_samples:
                 raise AttributeError("wrong number of posterior models")
 
         self.to(device)
@@ -202,17 +202,20 @@ class BNN(PyroModule):
 
                 basenet_copy = copy.deepcopy(self.basenet)
                 basenet_copy.load_state_dict(avg_state_dict)
-                preds = [basenet_copy.forward(inputs, layer_idx=layer_idx, *args, **kwargs)]
+                out = basenet_copy.forward(inputs, layer_idx=layer_idx, *args, **kwargs)
+                out = nnf.softmax(out, dim=-1)
+                preds = [out]
 
             else:
 
                 preds = []  
 
                 if training:
-
-                    for _ in range(n_samples):
-                        guide_trace = poutine.trace(self.guide).get_trace(inputs)   
-                        preds.append(guide_trace.nodes['_RETURN']['value'])
+                    # for _ in range(n_samples):
+                    guide_trace = poutine.trace(self.guide).get_trace(inputs)  
+                    out = guide_trace.nodes['_RETURN']['value']
+                    out = nnf.softmax(out, dim=-1)
+                    preds.append(out)
 
                 else:
                     for seed in sample_idxs:
@@ -230,7 +233,9 @@ class BNN(PyroModule):
 
                         basenet_copy = copy.deepcopy(self.basenet)
                         basenet_copy.load_state_dict(weights)
-                        preds.append(basenet_copy.forward(inputs, layer_idx=layer_idx, *args, **kwargs))
+                        out = basenet_copy.forward(inputs, layer_idx=layer_idx, *args, **kwargs)
+                        out = nnf.softmax(out, dim=-1)
+                        preds.append(out)
 
         elif self.inference == "hmc":
 
@@ -251,15 +256,18 @@ class BNN(PyroModule):
 
                 basenet_copy = copy.deepcopy(self.basenet)
                 basenet_copy.load_state_dict(avg_state_dict)
-                preds = [basenet_copy.forward(inputs, layer_idx=layer_idx, *args, **kwargs)]
+                out = basenet_copy.forward(inputs, layer_idx=layer_idx, *args, **kwargs)
+                out = nnf.softmax(out, dim=-1)
+                preds = [out]
 
             else:
-
                 preds = []
                 posterior_predictive = self.posterior_samples
                 for seed in sample_idxs:
                     net = posterior_predictive[seed]
-                    preds.append(net.forward(inputs, layer_idx=layer_idx, *args, **kwargs))
+                    out = basenet_copy.forward(inputs, layer_idx=layer_idx, *args, **kwargs)
+                    out = nnf.softmax(out, dim=-1)
+                    preds.append(out)
         
         logits = torch.stack(preds)
         return logits.mean(0) if expected_out else logits
@@ -322,7 +330,7 @@ class BNN(PyroModule):
                 y_batch = y_batch.to(device)
                 loss += svi.step(x_data=x_batch, y_data=y_batch.argmax(dim=-1))
 
-                outputs = self.forward(x_batch, n_samples=10, training=True, avg_posterior=False).to(device)
+                outputs = self.forward(x_batch, training=True, avg_posterior=False).to(device)
                 predictions = outputs.argmax(-1)
                 labels = y_batch.argmax(-1)
                 correct_predictions += (predictions == labels).sum().item()

src/utils/lrp.py
@@ -78,8 +78,9 @@ def compute_explanations(x_test, network, rule, normalize, n_samples=None, layer
 			y_hat = network.forward(x.unsqueeze(0), explain=True, rule=rule, layer_idx=layer_idx,
 									avg_posterior=avg_posterior)
 
-			if layer_idx==-1 or layer_idx==network.n_layers:
-				y_hat = nnf.softmax(y_hat, dim=-1)
+			if n_samples is None: # deterministic case
+				if layer_idx==-1 or layer_idx==network.n_layers:
+					y_hat = nnf.softmax(y_hat, dim=-1)
 
 			# Choose argmax
 			y_hat = y_hat[torch.arange(x.shape[0]), y_hat.max(1)[1]].sum()
@@ -103,8 +104,8 @@ def compute_explanations(x_test, network, rule, normalize, n_samples=None, layer
 			y_hat = network.forward(inputs=x_copy, n_samples=n_samples,
 									explain=True, rule=rule, layer_idx=layer_idx)
 
-			if layer_idx==-1 or layer_idx==network.n_layers:
-				y_hat = nnf.softmax(y_hat, dim=-1)
+			# if layer_idx==-1 or layer_idx==network.n_layers:
+			# 	y_hat = nnf.softmax(y_hat, dim=-1)
 
 			# Choose argmax
 			y_hat = y_hat[torch.arange(x_copy.shape[0]), y_hat.max(1)[1]]
@@ -216,6 +217,9 @@ def compute_vanishing_norm_idxs(inputs, n_samples_list, norm="linfty"):
 	return vanishing_norm_idxs, non_null_idxs
 
 def lrp_distances(original_heatmaps, adversarial_heatmaps, pxl_idxs=None, axis_norm=0):
+
+	if original_heatmaps.shape[0]==0:
+		return torch.empty(0)
 
 	original_heatmaps = original_heatmaps.reshape(*original_heatmaps.shape[:1], -1)
 	adversarial_heatmaps = adversarial_heatmaps.reshape(*adversarial_heatmaps.shape[:1], -1)